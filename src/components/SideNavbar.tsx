f75dd826a858158068c82cfc59e2f0627a7998ab
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-08-15 14:39:10 +0200

    code simplified and styles impoved

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -1,10 +1,5 @@
-import NavItem from "./NavItem";
-
-import { CiSearch } from "react-icons/ci";
-import { BsFillInboxFill } from "react-icons/bs";
-import { TiHome } from "react-icons/ti";
-import { IoMdMail } from "react-icons/io";
-import { IoIosSend } from "react-icons/io";
+import { NAV_ITEMS } from "../data";
+import Logo from "../assests/Logo.png";
 
 export default function SideNavbar({
   tabSelected,
@@ -12,32 +7,28 @@ export default function SideNavbar({
   tabSelected: (path: string) => void;
 }) {
   return (
-    <div className="flex flex-col justify-between p-4 dark:bg-[#101113] bg-white h-screen w-14  items-center py-6 border-r-2 dark:border-[#343A40] border-[#E0E0E0] left-0 top-0 fixed z-10">
-      <div className="rounded-xl">
+    <div className="flex flex-col justify-between p-4 dark:bg-[#101113] bg-white h-screen w-16  items-center border-r-2 dark:border-[#343A40] border-[#E0E0E0] left-0 top-0 fixed z-10">
+      <div>
         <img
-          src="./Logo.png"
-          className="h-8 rounded-xl object-left overflow-visible"
+          src={Logo}
+          className="h-8 rounded-xl object-left overflow-visible ml-3 mb-10"
           alt="Logo"
         />
+        <div className="pt-5 text-[#5a5a67]">
+          {NAV_ITEMS.map((item) => {
+            return (
+              <div
+                className="text-2xl p-2 m-2 hover:bg-orange-100 rounded-lg"
+                title={item.btnCalled}
+                onClick={() => tabSelected(item.path)}
+              >
+                {<item.icon />}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="flex flex-col justify-between">
-        <NavItem btName="home" path="/" userOpenedTab={tabSelected}>
-          <TiHome />
-        </NavItem>
-        <NavItem btName="search" path="/search" userOpenedTab={tabSelected}>
-          <CiSearch />
-        </NavItem>
-        <NavItem btName="inbox" path="/inbox" userOpenedTab={tabSelected}>
-          <IoMdMail />
-        </NavItem>
-        <NavItem btName="mail" path="/mail" userOpenedTab={tabSelected}>
-          <BsFillInboxFill />
-        </NavItem>
-        <NavItem btName="send" path="/send" userOpenedTab={tabSelected}>
-          <IoIosSend />
-        </NavItem>
-      </div>
-      <div className="text-white bg-amber-400 p-2 rounded-full">FP</div>
+      <div className="text-white bg-green-800 px-3 py-2 rounded-full">FP</div>
     </div>
   );
 }
--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,53 @@
+import { RiUserSearchFill } from "react-icons/ri";
+import { BsFillInboxFill } from "react-icons/bs";
+import { TiHome } from "react-icons/ti";
+import { IoMdMail } from "react-icons/io";
+import { FaThList } from "react-icons/fa";
+import { IoIosSend } from "react-icons/io";
+import { MdBarChart } from "react-icons/md";
 import { FiSend } from "react-icons/fi";
 import { IoIosMailOpen } from "react-icons/io";
+
+export const NAV_ITEMS = [
+    {
+        btnCalled: "home",
+        path: "/",
+        icon: TiHome,
+    },
+    {
+        btnCalled: "search",
+        path: "/search",
+        icon: RiUserSearchFill,
+    },
+    {
+        btnCalled: "new mail",
+        path: "/mail",
+        icon: IoMdMail,
+    },
+    {
+        btnCalled: "send mail",
+        path: "/send",
+        icon: IoIosSend,
+    },
+    {
+        btnCalled: "categories",
+        path: "/categories",
+        icon: FaThList,
+    },
+
+    {
+        btnCalled: "inbox",
+        path: "/inbox",
+        icon: BsFillInboxFill,
+    },
+    {
+        btnCalled: "usage",
+        path: "/usage",
+        icon: MdBarChart,
+    },
+
+]
+
 
 export const DUMMY_LEADDETAILS = {
     Name: "Orlando",
