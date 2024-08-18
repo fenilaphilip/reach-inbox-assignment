dd52f1f996154c47c6be2dfc4f90d7a2dfd548cc
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-08-18 15:24:05 +0200

    implemented Theme representation sun and moon

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 
+import { FaMoon } from "react-icons/fa";
+import { IoIosSunny } from "react-icons/io";
+
 export default function ThemeToggle() {
   const [darkMode, setDarkMode] = useState(false);
   useEffect(() => {
@@ -17,15 +20,16 @@ export default function ThemeToggle() {
     <>
       <label className="flex cursor-pointer select-none items-center">
         <div className="relative ">
-          <input
-            type="checkbox"
-            checked={darkMode}
-            onChange={togglehandler}
-            className="sr-only"
-          />
-
-          <div className="block h-6 w-12 rounded-full bg-[#c6c7cb]"></div>
-          <div className="dot absolute left-1 top-1 h-4 w-4 rounded-full bg-white transition"></div>
+          <button
+            type="button"
+            onClick={togglehandler}
+            aria-label={darkMode ? "Toggle Dark Theme" : "Toggle Light Theme"}
+            className="bg-[#c6c7cb] dark:bg-gray-900 w-14 h-4 xl:w-14 xl:h-7 rounded-full p-1 flex justify-between items-center transition duration-300 ease-out"
+          >
+            {!darkMode && <FaMoon className="text-[#e6f336]" size={18} />}
+            <div className="bg-white w-4 h-4 xl:w-5 xl:h-5 rounded-full shadow-md"></div>
+            {darkMode && <IoIosSunny className="text-secondary" size={20} />}
+          </button>
         </div>
       </label>
     </>
